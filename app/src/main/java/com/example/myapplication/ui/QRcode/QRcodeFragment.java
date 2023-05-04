6ce55772413021595e553335eea154a43059e934
rydnjs98 <98crace@gmail.com>
2023-05-04 21:41:18 +0900

    QRcode 완성

--- app/src/main/java/com/example/myapplication/ui/QRcode/QRcodeFragment.java
+++ app/src/main/java/com/example/myapplication/ui/QRcode/QRcodeFragment.java
@@ -1,10 +1,20 @@
 package com.example.myapplication.ui.QRcode;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+import com.example.myapplication.User;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,18 +25,26 @@ import com.example.myapplication.databinding.FragmentQrcodeBinding;
 public class QRcodeFragment extends Fragment {
 
     private FragmentQrcodeBinding binding;
+    private ImageView iv;
+    private String text = User.getStudentId();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        QRcodeViewModel qrcodeViewModel =
-                new ViewModelProvider(this).get(QRcodeViewModel.class);
+        View v = inflater.inflate(R.layout.fragment_qrcode, container, false);
 
-        binding = FragmentQrcodeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        iv = (ImageView) v.findViewById(R.id.qrcode);
 
-        final TextView textView = binding.textQrcode;
-        qrcodeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE, 200, 200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            iv.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return v;
     }
 
     @Override
--- app/src/main/java/com/example/myapplication/ui/QRcode/QRcodeViewModel.java
+++ app/src/main/java/com/example/myapplication/ui/QRcode/QRcodeViewModel.java
@@ -1,19 +1,29 @@
 package com.example.myapplication.ui.QRcode;
 
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.myapplication.R;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
 public class QRcodeViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
+
 
     public QRcodeViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is QRcode fragment");
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public void getQRcode() {
+
     }
 }
