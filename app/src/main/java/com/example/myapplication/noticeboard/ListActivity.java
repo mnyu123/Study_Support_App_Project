054fbfeef52ad7c3d8c513a3dba7f96c05a5fddf
sejun0622 <psj06222@naver.com>
2023-05-11 23:25:59 +0900

    게시판 글 작성 - 글 목록 클릭시 댓글작성가능

--- app/src/main/java/com/example/myapplication/noticeboard/ListActivity.java
+++ app/src/main/java/com/example/myapplication/noticeboard/ListActivity.java
@@ -1,250 +1,83 @@
 package com.example.myapplication.noticeboard;
 
-import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
-import com.example.myapplication.noticeboard.RegisterActivity;
+import android.widget.SimpleAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
 
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-
-import javax.crypto.Cipher;
-import javax.net.ssl.HttpsURLConnection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ListActivity extends AppCompatActivity {
 
-    // 로그에 사용할 TAG 변수
-    final private String TAG = getClass().getSimpleName();
-
-    // 사용할 컴포넌트 선언
-    ListView listView;
-
-    Button reg_button;
-    Context cscontext;
-    String userid = "";
-
-    // 리스트뷰에 사용할 제목 배열
-    ArrayList<String> titleList = new ArrayList<>();
-
-    // 클릭했을 때 어떤 게시물을 클릭했는지 게시물 번호를 담기 위한 배열
-    ArrayList<String> seqList = new ArrayList<>();
+    private ListView listView;
+    private List<String> titles = new ArrayList<>();
+    private List<String> contents = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        cscontext = this;
+        listView = findViewById(R.id.listView);
 
-        // LoginActivity 에서 넘긴 userid 값 받기
-        userid = getIntent().getStringExtra("userid");
+        // ListActivity에 등록된 글 가져오기
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            String title = bundle.getString("title");
+            String content = bundle.getString("content");
+            titles.add(title);
+            contents.add(content);
+        }
 
-        // 버튼 객체 초기화
-        reg_button = findViewById(R.id.reg_button);
+        // ListView에 어댑터 설정
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titles);
+        listView.setAdapter(adapter);
 
-        reg_button.setOnClickListener(new View.OnClickListener() {
+        // ListView 클릭 시 DetailActivity로 이동하고, 해당 아이템의 제목과 내용 전달
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                // RegisterActivity 로 이동
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(ListActivity.this, DetailActivity.class);
+                intent.putExtra("title", titles.get(position));
+                intent.putExtra("content", contents.get(position));
+                startActivity(intent);
+            }
+        });
+
+        Button register_btn = findViewById(R.id.reg_button);
+        register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Intent intent = new Intent(ListActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
     }
 
-
-    // onResume() 은 해당 액티비티가 화면에 나타날 때 호출됨
-    @Override
-    protected void onResume() {
-        super.onResume();
-// 해당 액티비티가 활성화 될 때, 게시물 리스트를 불러오는 함수를 호출
-        GetBoard getBoard = new GetBoard();
-        getBoard.execute();
+    public void addBoardItem(String title, String content, String userId, String regDate) {
+        titles.add(title);
+        contents.add(content);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titles);
+        listView.setAdapter(adapter);
     }
-
-
-    // 게시물 리스트를 읽어오는 함수
-    class GetBoard extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-            Log.d(TAG, "onPreExecute");
-        }
-
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            Log.d(TAG, "onPostExecute, " + result);
-
-// 배열들 초기화
-            titleList.clear();
-            seqList.clear();
-
-            try {
-
-// 결과물이 JSONArray 형태로 넘어오기 때문에 파싱
-                JSONArray jsonArray = new JSONArray(result);
-
-                for(int i=0;i<jsonArray.length();i++){
-                    JSONObject jsonObject = jsonArray.getJSONObject(i);
-
-                    String title = jsonObject.optString("title");
-                    String seq = jsonObject.optString("seq");
-
-// title, seq 값을 변수로 받아서 배열에 추가
-                    titleList.add(title);
-                    seqList.add(seq);
-
-                }
-
-// ListView 에서 사용할 arrayAdapter를 생성하고, ListView 와 연결
-                ArrayAdapter arrayAdapter = new ArrayAdapter<String>(ListActivity.this, android.R.layout.simple_list_item_1, titleList);
-                listView.setAdapter(arrayAdapter);
-
-// arrayAdapter의 데이터가 변경되었을때 새로고침
-                arrayAdapter.notifyDataSetChanged();
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-
-        }
-
-
-        @Override
-        protected String doInBackground(String... params) {
-//
-// String userid = params[0];
-// String passwd = params[1];
-
-            String server_url = "http://15.164.252.136/load_board.php";
-
-
-            URL url;
-            String response = "";
-            try {
-                url = new URL(server_url);
-
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(15000);
-                conn.setConnectTimeout(15000);
-                conn.setRequestMethod("POST");
-                conn.setDoInput(true);
-                conn.setDoOutput(true);
-                Uri.Builder builder = new Uri.Builder()
-                        .appendQueryParameter("userid", "");
-// .appendQueryParameter("passwd", passwd);
-                String query = builder.build().getEncodedQuery();
-
-                OutputStream os = conn.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(
-                        new OutputStreamWriter(os, "UTF-8"));
-                writer.write(query);
-                writer.flush();
-                writer.close();
-                os.close();
-
-                conn.connect();
-                int responseCode=conn.getResponseCode();
-
-                if (responseCode == HttpsURLConnection.HTTP_OK) {
-                    String line;
-                    BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    while ((line=br.readLine()) != null) {
-                        response+=line;
-                    }
-                }
-                else {
-                    response="";
-
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            return response;
-        }
-    }
-
-
-        protected String doInBackground(String... params) {
-//
-// String userid = params[0];
-// String passwd = params[1];
-
-            String server_url = "http://15.164.252.136/load_board.php";
-
-
-            URL url;
-            String response = "";
-            try {
-                url = new URL(server_url);
-
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(15000);
-                conn.setConnectTimeout(15000);
-                conn.setRequestMethod("POST");
-                conn.setDoInput(true);
-                conn.setDoOutput(true);
-                Uri.Builder builder = new Uri.Builder()
-                        .appendQueryParameter("userid", "");
-// .appendQueryParameter("passwd", passwd);
-                String query = builder.build().getEncodedQuery();
-
-                OutputStream os = conn.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(
-                        new OutputStreamWriter(os, "UTF-8"));
-                writer.write(query);
-                writer.flush();
-                writer.close();
-                os.close();
-
-                conn.connect();
-                int responseCode=conn.getResponseCode();
-
-                if (responseCode == HttpsURLConnection.HTTP_OK) {
-                    String line;
-                    BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    while ((line=br.readLine()) != null) {
-                        response+=line;
-                    }
-                }
-                else {
-                    response="";
-
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            return response;
-        }
-    }
-
+}
--- app/src/main/java/com/example/myapplication/noticeboard/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/noticeboard/RegisterActivity.java
@@ -3,19 +3,66 @@ package com.example.myapplication.noticeboard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Serializable;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Random;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class RegisterActivity extends AppCompatActivity {
+    private EditText title_et, content_et;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        title_et = findViewById(R.id.title_et);
+        content_et = findViewById(R.id.content_et);
+
+        Button submit_btn = findViewById(R.id.reg_button);
+        submit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = title_et.getText().toString();
+                String content = content_et.getText().toString();
+
+                // DetailActivity로 작성한 글 내용 전달
+                Intent intent = new Intent(RegisterActivity.this, ListActivity.class);
+                intent.putExtra("title", title);
+                intent.putExtra("content", content);
+                startActivity(intent);
+                finish(); // RegisterActivity 종료
+            }
+        });
     }
 }
